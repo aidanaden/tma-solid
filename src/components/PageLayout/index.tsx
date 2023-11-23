0dca4a9765e6f97b2524148e8adb668d1447afc2
aidanaden <aidan@u.nus.edu>
2023-11-24 03:30:44 +0800

    fix: remove pagelayout css

--- src/components/PageLayout/index.tsx
+++ src/components/PageLayout/index.tsx
@@ -3,7 +3,7 @@ import { Component, ComponentProps, splitProps } from "solid-js";
 export const PageLayout: Component<ComponentProps<"div">> = (props) => {
   const [local, others] = splitProps(props, ["children"]);
   return (
-    <div class="p-3 bg-neutral-100 text-neutral-800" {...others}>
+    <div class="p-3" {...others}>
       {local.children}
     </div>
   );
