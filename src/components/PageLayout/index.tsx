8f41ff409ef3c0630121ec96733dc13c46325cbb
aidanaden <aidan@u.nus.edu>
2023-11-24 07:43:15 +0800

    twind

--- src/components/PageLayout/index.tsx
+++ src/components/PageLayout/index.tsx
@@ -3,7 +3,7 @@ import { Component, ComponentProps, splitProps } from "solid-js";
 export const PageLayout: Component<ComponentProps<"div">> = (props) => {
   const [local, others] = splitProps(props, ["children"]);
   return (
-    <div class="p-3 bg-tg-bg text-tg-text" {...others}>
+    <div class="p-3" {...others}>
       {local.children}
     </div>
   );
